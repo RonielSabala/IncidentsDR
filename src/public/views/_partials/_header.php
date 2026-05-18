c45f862eff678f1742ef284a0e14dea6e30d3926
RonielSabala <ronielsabala11@gmail.com>
2026-05-17 23:30:23 -0400

    fix: window project name

--- src/public/views/_partials/_header.php
+++ src/public/views/_partials/_header.php
@@ -12,7 +12,7 @@ $homePage = GeneralUtils::getUserDefaultRouteByRole($userRole);
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Proyecto final. Grupo No. 1</title>
+    <title>IncidentsDR</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;800&family=Playfair+Display:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
